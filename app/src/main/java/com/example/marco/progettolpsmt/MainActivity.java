2ee750a06a3ef17723995205d39c27ce60f7ad3f
Marco Roberti <marco.roberti@studenti.unitn.it>
2017-08-09 22:47:14 +0200

    Trying to init the timer

--- app/src/main/java/com/example/marco/progettolpsmt/MainActivity.java
+++ app/src/main/java/com/example/marco/progettolpsmt/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.marco.progettolpsmt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CircularProgressBar studytimer = (CircularProgressBar) findViewById(R.id.study);
+
+        studytimer.animateProgressTo(0, 100, new CircularProgressBar.ProgressAnimationListener() {
+
+            @Override
+            public void onAnimationStart() {
+            }
+
+            @Override
+            public void onAnimationProgress(int progress) {
+                studytimer.setTitle(progress + "%");
+            }
+
+            @Override
+            public void onAnimationFinish() {
+                studytimer.setSubTitle("done");
+            }
+        });
+
+        /*Intent intent = new Intent(this, TimerActivity.class);
+        startActivity(intent);*/
     }
 }
